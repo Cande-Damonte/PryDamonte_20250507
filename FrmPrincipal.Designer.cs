1c78a1c4eb658dacccf5b11e03bfe99d9b3edbd0
Alumno <Alumno@PC39>
2025-05-07 12:51:32 -0300

    Agregar archivos de proyecto.

---
+++ FrmPrincipal.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace PryDamonte_20250507
+{
+    partial class FrmPrincipal
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.CmbSuperHeroes = new System.Windows.Forms.ComboBox();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.pctSuperheroe = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pctSuperheroe)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(104, 26);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(93, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Superheroe";
+            // 
+            // CmbSuperHeroes
+            // 
+            this.CmbSuperHeroes.FormattingEnabled = true;
+            this.CmbSuperHeroes.Location = new System.Drawing.Point(62, 51);
+            this.CmbSuperHeroes.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.CmbSuperHeroes.Name = "CmbSuperHeroes";
+            this.CmbSuperHeroes.Size = new System.Drawing.Size(180, 28);
+            this.CmbSuperHeroes.TabIndex = 1;
+            // 
+            // imageList1
+            // 
+            this.imageList1.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit;
+            this.imageList1.ImageSize = new System.Drawing.Size(16, 16);
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // pctSuperheroe
+            // 
+            this.pctSuperheroe.Location = new System.Drawing.Point(48, 96);
+            this.pctSuperheroe.Name = "pctSuperheroe";
+            this.pctSuperheroe.Size = new System.Drawing.Size(211, 133);
+            this.pctSuperheroe.TabIndex = 2;
+            this.pctSuperheroe.TabStop = false;
+            // 
+            // FrmPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(320, 274);
+            this.Controls.Add(this.pctSuperheroe);
+            this.Controls.Add(this.CmbSuperHeroes);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "FrmPrincipal";
+            this.Text = "Principal";
+            ((System.ComponentModel.ISupportInitialize)(this.pctSuperheroe)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox CmbSuperHeroes;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.PictureBox pctSuperheroe;
+    }
+}
+
